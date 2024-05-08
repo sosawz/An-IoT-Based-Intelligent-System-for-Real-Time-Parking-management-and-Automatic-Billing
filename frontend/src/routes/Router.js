02600e1c1ef3b3cea4c4c673543c51dcf33ef4b5
Oorawan Sukngam <sosawz.sng@gmail.com>
2024-05-09 05:00:27 +0700

    Update Router.js

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,4 +1,5 @@
 // import { exact } from "prop-types";
+import { element, exact } from "prop-types";
 import { lazy } from "react";
 import { Navigate } from "react-router-dom";
 
@@ -10,6 +11,7 @@ const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
 const Starter = lazy(() => import("../views/Starter.js"));
 const Admin = lazy(() => import("../views/Admin.js"));
 const About = lazy(() => import("../views/About.js"));
+const Payment = lazy(() => import("../views/Payment.js"));
 
 /*****Routes******/
 
@@ -22,6 +24,7 @@ const ThemeRoutes = [
       { path: "/starter", exact: true, element: <Starter /> },
       { path: "/admin", exact: true, element: <Admin/> },
       { path: "/about", exact: true, element: <About /> },
+      { path: "/payment", exact: true, element: <Payment/>},
     ],
   },
 ];
