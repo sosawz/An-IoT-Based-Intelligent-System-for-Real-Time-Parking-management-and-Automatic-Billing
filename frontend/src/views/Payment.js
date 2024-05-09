e71a4029424b71075deec0372371a1bde1a5a5be
Oorawan Sukngam <sosawz.sng@gmail.com>
2024-05-10 03:11:55 +0700

    Update Bill

--- frontend/src/views/Payment.js
+++ frontend/src/views/Payment.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { format } from 'date-fns';
-
+import Receipt from '../components/Receipt';
 const Payment = () => {
     const [data, setData] = useState([]);
     const [searchQuery, setSearchQuery] = useState("");
@@ -92,7 +92,6 @@ const Payment = () => {
     }
 
     const handleDownload = () => {
-        alert("Downloading receipt...");
         // Logic for downloading the receipt
     }
 
@@ -162,47 +161,10 @@ const Payment = () => {
                         <p>No matching plates found.</p>
                     </div>
                 )}
-                {showReceipt && (
-                    <div className="card">
-                        <div className="card-body mx-4">
-                            <div className="container">
-                                <p className="my-5 text-center" style={{ fontSize: 30 }}>Parking Bill</p>
-                                <div className="row">
-                                    <ul className="list-unstyled">
-                                        <li className="text-black">Parking Payment</li>
-                                        <li className="text-muted mt-1"><span className="text-black">Entry Time:</span> {format(new Date(entryTime), 'M/d/yyyy, hh:mm:ss a')}</li>
-                                        <li className="text-muted mt-1"><span className="text-black">Exit Time:</span> {exitTime}</li>
-                                        {plateNumber && <li className="text-muted mt-1"><span className="text-black">Plate Number:</span> {plateNumber}</li>}
-                                        {customerName && <li className="text-muted mt-1"><span className="text-black">Customer Name:</span> {customerName}</li>}
-                                    </ul>
-                                    <hr />
-                                    <div className="col-xl-10">
-                                        <p>Parking Fee</p>
-                                    </div>
-                                    <div className="col-xl-2">
-                                        <p className="float-end">{serviceCharge}฿</p>
-                                    </div>
-                                    <hr />
-                                </div>
-                                <div className="row text-black">
-                                    <div className="col-xl-12">
-                                        <p className="float-end fw-bold">Total: {serviceCharge}฿</p>
-                                    </div>
-                                    <hr style={{ border: '2px solid black' }} />
-                                </div>
-                            </div>
-                            <div className="text-center mt-4">
-                                <a href="#" onClick={handleDownload} className="btn btn-primary me-3">Download</a>
-                                <button onClick={handleShare} className="btn btn-success">Share</button>
-                            </div>
-                        </div>
-                    </div>
-                )}
-
+                {showReceipt && <Receipt entryTime={entryTime} exitTime={exitTime} plateNumber={plateNumber} customerName={customerName} serviceCharge={serviceCharge} handleDownload={handleDownload} handleShare={handleShare} />}
             </div>
         </section>
     );
-
 }
 
 export default Payment;
