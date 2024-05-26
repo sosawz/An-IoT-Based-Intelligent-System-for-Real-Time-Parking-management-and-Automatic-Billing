36a22b556f3f65bd242366c64bc171538bd87bcd
Oorawan Sukngam <sosawz.sng@gmail.com>
2024-05-26 20:36:11 +0700

    Update edit.js

--- frontend/src/views/edit.js
+++ frontend/src/views/edit.js
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const Edit = (props) => {
+const Edit = () => {
+    const { id } = useParams(); // Use useParams to get the dynamic id
     const navigate = useNavigate();
     const [plateData, setPlateData] = useState({
-        id: props.match.params.id,
         plate: '',
         image: '',
         timestamp: '',
-        redirect: null,
     });
 
     useEffect(() => {
         axios
-            .get(`http://localhost:8081/admin/edit-plates/${plateData.id}`)
+            .get(`http://localhost:8081/admin/edit-plates/${id}`)
             .then((res) => {
                 let data = res.data.data[0];
                 console.log(data);
@@ -28,15 +27,14 @@ const Edit = (props) => {
             .catch((error) => {
                 console.log(error);
             });
-    }, [plateData.id]);
+    }, [id]);
 
     const handleChange = (e) => {
-        console.log(e.target.name, e.target.value);
         let name = e.target.name;
         let value = e.target.value;
         setPlateData(prevPlateData => ({
             ...prevPlateData,
-            [name]: value
+            [name]: value,
         }));
     };
 
@@ -45,6 +43,9 @@ const Edit = (props) => {
         axios.post('http://localhost:8081/admin/edit-plates', plateData)
             .then(res => {
                 if (res.data.result) {
+                    setPlateData(prevPlateData => ({
+                        ...prevPlateData
+                    }))
                     navigate('/admin');
                 }
             })
@@ -53,42 +54,14 @@ const Edit = (props) => {
             });
     };
 
-
     return (
         <section>
-            <section
-                className="hero-wrap hero-wrap-2 js-fullheight"
-                style={{
-                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${plateData.image})`,
-                    height: "703px",
-                }}
-            >
-                <div className="container">
-                    <div className="row no-gutters slider-text js-fullheight align-items-end justify-content-center" style={{ height: "703px" }}>
-                        <div className="col-md-9 ftco-animate pb-5 text-center fadeInUp ftco-animated">
-                            <p className="breadcrumbs">
-                                <span className="mr-2">
-                                    <Link to="/">Home <i className="fa fa-chevron-right"></i></Link>
-                                </span>
-                                <span className="mr-2">
-                                    <Link to="/admin">Admin</Link> <i className="fa fa-chevron-right"></i>
-                                </span>
-                                <span className="mr-2">
-                                    <Link to="/edit">Edit</Link>
-                                </span>
-                            </p>
-                            <h1 className="mb-0 bread">Edit Plate</h1>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
             <div className="container py-5">
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card shadow-lg border-0 rounded-lg">
                             <div className="card-header bg-primary text-white rounded-top">
-                                <h3 className="mb-0 thai">Edit Plate #id {plateData.id}</h3>
+                                <h3 className="mb-0 thai">Edit Plate #id {id}</h3>
                             </div>
                             <div className="card-body thai">
                                 <form onSubmit={handleSubmit}>
